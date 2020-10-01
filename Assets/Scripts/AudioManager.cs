acd0b8626dfb04a40db4ac08f1ce5d69b026f576
Stephen C <slc180002@utdalla.edu>
2020-10-01 00:36:18 -0500

    Finished Setup Document A
    Now has audio player, music, reset data functionality, quit functionality, and high score.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    //Singleton Pattern
+    public static AudioManager Instance = null;
+
+    AudioSource _audioSource;
+
+    private void Awake()
+    {
+        #region Singleton Pattern (Simple)
+        if(Instance == null)
+        {
+            //doesn't exist yet, this is now our singleton!
+            Instance = this;
+            DontDestroyOnLoad(this);
+            // fill references
+            _audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+    }
+
+    public void PlaySong(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Level01Controller : MonoBehaviour
 {
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
     private void Update()
     {
+        //Increase Score
+        //TODO replace with real implementation later
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        //Exit Level
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             ExitLevel();
@@ -14,6 +25,23 @@ public class Level01Controller : MonoBehaviour
     }
     public void ExitLevel()
     {
+        //compare score to high score
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if(_currentScore > highScore)
+        {
+            //save current score as high score
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+        //load main menu
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        //increase score
+        _currentScore += scoreIncrease;
+        //update score display so we can see the new score
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
     }
 }
